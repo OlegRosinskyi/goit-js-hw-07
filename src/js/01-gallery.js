92159a635956f79f6ab138f7ad64c8d360ce6e5a
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-11-18 21:02:17 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,19 +7,29 @@ const galleryEl = document.querySelector(".gallery");
 
 const lastInLiImageEl = galleryItems
   .map(({ preview, original, description }) =>
-  `<li class = "gallery__item item"> <img class="gallery__image imgformat"  src="${original}" loading="lazy" alt="${description}"width="850" ></li>`)
+      `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${original}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>
+ `)
     .join("");
-galleryEl.innerHTML = '<ul class="gallery list">' + lastInLiImageEl + '</ul>';  
+galleryEl.innerHTML = lastInLiImageEl;  
 
 //galleryEl.insertAdjacentHTML('beforeend', '<ul class="list">' + lastInLiImageEl + '</ul>');  
 
 
 const selectImg = event => {
-    
+    event.preventDefault();
     if (event.target.nodeName !== "IMG") { return; }
    // const selectimg = event.target;
    // selectimg.classList.add("sel-img");
-   // const selectedAlt = event.target.alt;
+   const selectedAlt = event.target.alt;
     const selectedImg = event.target.src;
     instance.show();
     const navImgInDivEl = document.querySelector('.basicLightbox__placeholder > img'); 
@@ -27,10 +37,10 @@ const selectImg = event => {
     // const galleryEl = document.querySelector('.gallery >li [alt="${selectedImg}"]');
     //galleryEl.classList.add("gallery__link");
    
-   // console.log(selectedAlt);
-   // console.log(selectedImg);
+   console.log(selectedAlt);
+   console.log(selectedImg);
     
-   // console.log(event.currentTarget); console.log(event.target);
+   console.log(event.currentTarget); console.log(event.target);
 };
 
 galleryEl.addEventListener("click", selectImg);
