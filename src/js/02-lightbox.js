625e2f558c5effad57d6873a07dd8cf3b66c2fe0
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-11-19 16:12:34 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -6,31 +6,15 @@ const galleryEl = document.querySelector(".gallery");
 
 const lastInLiImageEl = galleryItems
   .map(({ preview, original, description }) =>
-        `<a class="gallery__item" href="large-image.jpg">
-         <img class="gallery__image" src="${original}" alt="${description}" />
-        </a>
+        ` <a class="gallery__item" href="${preview}">
+         <img class="gallery__image" src="${original}" alt="${description} title="Beautiful Image" />
+        </a> 
         `)
     .join("");
-galleryEl.innerHTML = lastInLiImageEl; 
-
-const selectImg = event => {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") { return; }
-   // const selectimg = event.target;
-   // selectimg.classList.add("sel-img");
-   const selectedAlt = event.target.alt;
-    const selectedImg = event.target.src;
-   // instance.show();
-   // const navImgInDivEl = document.querySelector('.basicLightbox__placeholder > img'); 
-   // navImgInDivEl.src = selectedImg;
-    // const galleryEl = document.querySelector('.gallery >li [alt="${selectedImg}"]');
-    //galleryEl.classList.add("gallery__link");
-   
-   console.log(selectedAlt);
-   console.log(selectedImg);
-    
-   console.log(event.currentTarget); console.log(event.target);
-};
-
-galleryEl.addEventListener("click", selectImg);
+galleryEl.innerHTML = '<div class="gallery">' + lastInLiImageEl + '</div> '; 
+//galleryEl.remove("ul");
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
 
